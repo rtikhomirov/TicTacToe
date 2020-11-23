468f6612d28d5719245a63bf9027718f46757b27
Roman Tikhomirov <r.tikhomirov@gmail.com>
2020-11-23 10:45:10 +0200

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Square extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className='container-fluid'>
+            </div>
+        );
+    }
+}
+export default Square;
